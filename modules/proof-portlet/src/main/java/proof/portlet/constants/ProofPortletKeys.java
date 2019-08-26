95b7b5eb380d5b6e71e3045a5c6c2799738bed61
Luismi Barcos <luismiguelbarco@liferay.com>
2019-08-26 16:21:39 +0200

    First commit

---
+++ modules/proof-portlet/src/main/java/proof/portlet/constants/ProofPortletKeys.java
@@ -0,0 +1,11 @@
+package proof.portlet.constants;
+
+/**
+ * @author luismiguelbarco
+ */
+public class ProofPortletKeys {
+
+	public static final String PROOF =
+		"proof_portlet_ProofPortlet";
+
+}
